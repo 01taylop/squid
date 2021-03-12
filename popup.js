6930824c155e17e02dffde7e192c025ad25bb5ec
Patrick Taylor <pattaylor@expediagroup.com>
2021-03-12 20:06:19 -0300

    Only initialise the content script once 🌟
    The popup script runs each time it is opened.
    This could lead to the same content script being added multiple
    times, which is far from ideal.
    This way, we initialise the script when the popup is first opened,
    and use messages to toggle the "inking" state.

--- popup.js
+++ popup.js
@@ -18,57 +18,76 @@ const setIsInking = inkingState => {
   })
 }
 
+const getTabId = async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+  return tab.id
+}
+
 // Init
-chrome.storage.local.get('isInking', ({ isInking }) => {
-  setIsInking(!!isInking)
-})
+document.addEventListener('DOMContentLoaded', async () => {
+  const tabId = await getTabId()
+
+  chrome.storage.local.get('isInking', ({ isInking }) => {
+    setIsInking(!!isInking)
+  })
+
+  chrome.scripting.executeScript({
+    target: {
+      tabId,
+    },
+    function: initSquid,
+  })
+}, false)
 
 // Extention Event Listeners
 inkContentButton.addEventListener('click', async () => {
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+  const tabId = await getTabId()
 
   if (isInking) {
     setIsInking(false)
-    chrome.tabs.sendMessage(tab.id, { id: 'END_INKING' })
+    chrome.tabs.sendMessage(tabId, { id: 'DISABLE_INKING' })
     return
   }
 
   setIsInking(true)
-  chrome.scripting.executeScript({
-    target: {
-      tabId: tab.id,
-    },
-    function: initInking,
-  })
+  chrome.tabs.sendMessage(tabId, { id: 'ENABLE_INKING' })
   window.close()
 })
 
 // Content Script Functions
-const initInking = () => {
-  const handleMouseMove = e => {
-    const element = document.elementFromPoint(e.clientX, e.clientY)
-    console.log(element)
-  }
+const initSquid = () => {
+  if (!window.SQUID) {
+    window.SQUID = {}
 
-  const handleKeydown = e => {
-    if (e.key === 'Escape') {
-      removeEventListeners()
-      chrome.storage.local.set({ isInking: false })
+    window.SQUID.handleMouseMove = e => {
+      const element = document.elementFromPoint(e.clientX, e.clientY)
+      console.log(element)
     }
-  }
 
-  const removeEventListeners = () => {
-    document.removeEventListener('mousemove', handleMouseMove)
-    document.removeEventListener('keydown', handleKeydown)
-  }
-
-  window.focus()
-  document.addEventListener('mousemove', handleMouseMove)
-  document.addEventListener('keydown', handleKeydown)
-
-  chrome.runtime.onMessage.addListener((request, _sender, _sendResponse) => {
-    if (request.id == 'END_INKING') {
-      removeEventListeners()
+    window.SQUID.handleKeydown = e => {
+      if (e.key === 'Escape') {
+        window.SQUID.disableInking()
+        chrome.storage.local.set({ isInking: false })
+      }
     }
-  })
+
+    window.SQUID.enableInking = () => {
+      window.focus()
+      document.addEventListener('mousemove', window.SQUID.handleMouseMove)
+      document.addEventListener('keydown', window.SQUID.handleKeydown)
+    }
+
+    window.SQUID.disableInking = () => {
+      document.removeEventListener('mousemove', window.SQUID.handleMouseMove)
+      document.removeEventListener('keydown', window.SQUID.handleKeydown)
+    }
+
+    chrome.runtime.onMessage.addListener((request, _sender, _sendResponse) => {
+      if (request.id == 'ENABLE_INKING') {
+        window.SQUID.enableInking()
+      } else if (request.id == 'DISABLE_INKING') {
+        window.SQUID.disableInking()
+      }
+    })
+  }
 }
